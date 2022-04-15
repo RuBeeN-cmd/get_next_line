7a95bf896c9b136d66d7ea53a9f1b3d79b1081f7
Ruben Rollin <rrollin@2D5.42angouleme.fr>
2022-04-15 13:59:15 +0200

    First commit

---
+++ get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rrollin <rrollin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/15 09:51:24 by rrollin           #+#    #+#             */
+/*   Updated: 2022/04/15 13:55:30 by rrollin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include<unistd.h>
+# include<stdlib.h>
+
+typedef struct s_dict_cell
+{
+	int					fd;
+	int					ret;
+	char				*buf;
+	struct s_dict_cell	*next;
+}	t_dict_cell;
+
+char		*get_next_line(int fd);
+t_dict_cell	*ft_lstget_cell(int fd, t_dict_cell **list);
+t_dict_cell	*ft_lstnew(int fd);
+void		ft_lstdel_one(t_dict_cell *cell, t_dict_cell **list);
+int			ft_chr(char *str, char c);
+int			ft_loop_read(int fd, t_dict_cell *cell);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_sub(char *s, int start, int len);
+int			ft_read(char *str, t_dict_cell *cell, int fd);
+char		*ft_start_read(int fd, t_dict_cell **list, t_dict_cell **tp);
+
+#endif
